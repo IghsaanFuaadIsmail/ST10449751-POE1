6694897705329445ab67362590c29fb2099611f0
RC_Student_lab <RC_Student_lab@10.0.0.85>
2024-09-16 06:48:27 +0000

    username conditions created

--- src/main/java/com/mycompany/poe1/POE1.java
+++ src/main/java/com/mycompany/poe1/POE1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.poe1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RC_Student_lab
@@ -11,6 +13,33 @@ package com.mycompany.poe1;
 public class POE1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        int count = 0;
+         String username;
+String password;
+String firstName;
+String lastName;
+String user;
+String pass;
+Scanner sc = new Scanner(System.in);
+System.out.println("Create your username");
+        username = sc.next();
+        
+        System.out.println("Create your password");
+        password = sc.next();
+        
+        System.out.println("Enter your first name");
+       firstName= sc.next();
+       
+       System.out.println("Enter your last name");
+        lastName= sc.next();
+        
+        if(username.contains("_") && username.length()<=5){
+            System.out.println("Username Succesfuly captured");    
+        }
+        else{
+            System.out.print("User name is not correctly formatted please ensure that your user name contains an underscore and is no more than 5 characters in length");
+        }
+
+                  sc.close();
+}
 }
